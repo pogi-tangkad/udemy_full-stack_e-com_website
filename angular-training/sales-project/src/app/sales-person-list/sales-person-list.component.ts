c74bc661cc940a02e84485936a47482a5a68f071
pogi-tangkad <pogi.tangkad@gmail.com>
2025-03-11 04:28:55 -0500

    A: create new angular project, create sales-person-list component and associated class, update component html pages to show list

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  standalone: false,
+  templateUrl: './sales-person-list.component.html',
+  styleUrl: './sales-person-list.component.css'
+})
+export class SalesPersonListComponent {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Pogi", "Tangkad", "pogi.tangkad@pogi.com", 50000),
+    new SalesPerson("Kevin", "Repking", "kevin.repking@pogi.com", 33000),
+    new SalesPerson("Mahal", "Ko", "mahal.ko@pogi.com", 17000),
+    new SalesPerson("Asawa", "Ganda", "asawa.ganda@pogi.com", 99000)
+  ];
+  
+
+  constructor() {}
+
+
+}
